ef00563390bfdb8acd1b4d3d282d9b5a029b1e48
Fran-Jos <fjchanataxi@uce.edu.ec>
2023-02-01 19:44:59 -0500

    Taller25

---
+++ src/test/java/com/example/demo/ProyectoPwU3Jy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPwU3Jy1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
